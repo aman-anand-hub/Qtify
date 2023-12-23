68c24d9d6edd548dfd1115e7ac4642db8127fee9
aman-anand-hub <amanngp01@gmail.com>
2023-12-23 17:20:01 +0530

    grid of cards

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./HeroSection.module.css";
-import {ReactComponent as Headphone} from "../../assets/headphone.svg"
+import Headphone from "../../assets/vibrating-headphone 1.png"
 
 function HeroSection() {
     return (
         <div className={styles.hero}>
-            <Headphone />
+            <div className={styles.heroTextDiv}>
+                <p>100 Thousand Songs, ad-free</p>
+                <p>Over thousands podcast episodes</p>
+            </div>
+            <img src={Headphone} alt="headphone"></img>
         </div>
     )
 }
